4a9d3d4ad47d72d4df8056d37fca06fdf69ea9e2
n30w <hello@neoalabastro.com>
2024-02-29 02:06:52 +0800

    refactor(next): inline homework map into return
    Removed one function.

--- frontend/src/app/components/Assignments.tsx
+++ frontend/src/app/components/Assignments.tsx
@@ -1,26 +1,30 @@
 const Assignments = () => {
-    const homeworks = [
-        {'name': 'Homework 1', 'duedate': 'Feb 8'},
-        {'name': 'Homework 2', 'duedate': 'Feb 25'},
-        {'name': 'Homework 3', 'duedate': 'March 3'},
-        {'name': 'Homework 4', 'duedate': 'March 10'}
-    ]
-
-    const hwElements = homeworks.map(homework => {
-        return (
-            <li>
-                <h2 class="text-m font-bold">{homework.name}</h2>
-                <p class="text-sm font-light pb-1 mb-1 border-b border-black">{homework.duedate}</p>
-            </li>
-        )
-    })
+  const homeworks = [
+    { name: "Homework 1", duedate: "Feb 8" },
+    { name: "Homework 2", duedate: "Feb 25" },
+    { name: "Homework 3", duedate: "March 3" },
+    { name: "Homework 4", duedate: "March 10" },
+  ];
 
   return (
-    <div class="w-60 h-70 px-4">
-        <h1 class="font-bold text-xl border-b-2 border-black mb-4 pb-4">Assignments</h1>
-        <ul>{hwElements}</ul>
+    <div className="w-60 h-70 px-4">
+      <h1 className="font-bold text-xl border-b-2 border-black mb-4 pb-4">
+        Assignments
+      </h1>
+      <ul>
+        {homeworks.map((homework, key) => {
+          return (
+            <li key={key}>
+              <h2 className="text-m font-bold">{homework.name}</h2>
+              <p className="text-sm font-light pb-1 mb-1 border-b border-black">
+                {homework.duedate}
+              </p>
+            </li>
+          );
+        })}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Assignments
+export default Assignments;
