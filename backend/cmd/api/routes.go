2d48bfb19ab232a6c3adcba78dbf4299e5a0b1ac
n30w <hello@neoalabastro.com>
2024-03-01 01:42:22 +0800

    feat(go): add new routes
    Add new routes to routes.go for user and login.
    
    Co-Authored-By: Michelle <125943298+hypnosushi@users.noreply.github.com>

--- backend/cmd/api/routes.go
+++ backend/cmd/api/routes.go
@@ -12,12 +12,19 @@ func (app *application) routes() *http.ServeMux {
 	router.HandleFunc("/v1/course/create", app.courseCreateHandler)
 	router.HandleFunc("/v1/course/read", app.courseReadHandler)
 	router.HandleFunc("/v1/course/update", app.courseUpdateHandler)
+	router.HandleFunc("/v1/course/delete", app.courseDeleteHandler)
 
 	// User CRUD operations
 	router.HandleFunc("/v1/user/create", app.userCreateHandler)
 	router.HandleFunc("/v1/user/read", app.userReadHandler)
 	router.HandleFunc("/v1/user/update", app.userUpdateHandler)
 	router.HandleFunc("/v1/user/delete", app.userDeleteHandler)
+
+	// A user posts something to a discussion
+	router.HandleFunc("/v1/user/post", app.userPostHandler)
+
+	// Login will require authorization, body will contain the credential info
+	router.HandleFunc("/v1/user/login", app.userLoginHandler)
 
 	// Assignment CRUD operations
 	router.HandleFunc("/v1/course/assignment/create", app.assignmentCreateHandler)
@@ -27,12 +34,9 @@ func (app *application) routes() *http.ServeMux {
 
 	// Discussion CRUD operations
 	router.HandleFunc("/v1/course/discussion/create", app.discussionCreateHandler)
-	router.HandleFunc("/v1/course/discussion/read" app.discussionReadHandler)
+	router.HandleFunc("/v1/course/discussion/read", app.discussionReadHandler)
 	router.HandleFunc("/v1/course/discussion/update", app.discussionUpdateHandler)
 	router.HandleFunc("/v1/course/discussion/delete", app.discussionDeleteHandler)
-
-	// Login will require authorization, body will contain the credential info
-	router.HandleFunc("/v1/user/login", app.userLoginHandler)
 
 	return router
 }
