58948e6514f0e5c4ca53e871566f6f894195b652
n30w <hello@neoalabastro.com>
2024-03-02 23:22:28 +0800

    refactor(go): healtcheck.go use writer
    healtcheck.go use writeJSON method.

--- backend/cmd/api/healthcheck.go
+++ backend/cmd/api/healthcheck.go
@@ -1,12 +1,21 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version: %s\n", version)
+
+	res := map[string]any{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
+
+	err := app.writeJSON(w, http.StatusOK, res, nil)
+
+	if err != nil {
+		app.logger.Print(err)
+		http.Error(w, "The server could not process your request", http.StatusInternalServerError)
+	}
 }
