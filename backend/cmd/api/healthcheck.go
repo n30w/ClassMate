bea5bd695cd3106e3787d88f88e94c17495ddb22
n30w <hello@neoalabastro.com>
2024-03-03 00:50:46 +0800

    refactor(go): update healthcheck

--- backend/cmd/api/healthcheck.go
+++ backend/cmd/api/healthcheck.go
@@ -4,18 +4,24 @@ import (
 	"net/http"
 )
 
+// healthCheckHandler handles healthcheck requests. It uses writeJSON()
+// to write system health data to the http.ResponseWriter stream.
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
 
-	res := map[string]any{
-		"status":      "available",
-		"environment": app.config.env,
-		"version":     version,
+	app.logger.Printf("received %s from %s", r.Method, r.RemoteAddr)
+
+	res := jsonWrap{
+		"status": "available",
+		"system_info": map[string]any{
+			"status":      "available",
+			"environment": app.config.env,
+			"version":     version,
+		},
 	}
 
 	err := app.writeJSON(w, http.StatusOK, res, nil)
 
 	if err != nil {
-		app.logger.Print(err)
-		http.Error(w, "The server could not process your request", http.StatusInternalServerError)
+		app.internalServerError(w, r, err)
 	}
 }
