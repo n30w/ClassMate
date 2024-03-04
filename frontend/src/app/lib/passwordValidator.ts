d3fc58650f9446450494876eb499b12caa2038ee
n30w <hello@neoalabastro.com>
2024-03-04 21:02:02 +0800

    feat(next): create passwordValidator
    Relocate validatePassword to lib directory

---
+++ frontend/src/app/lib/passwordValidator.ts
@@ -0,0 +1,6 @@
+const validatePassword = (password: string): boolean => {
+  const regex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+  return regex.test(password);
+};
+
+export default validatePassword;
