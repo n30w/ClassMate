5021e55527a75fda131c75a93022286aac66c267
reesedychiao <83895576+reesedychiao@users.noreply.github.com>
2024-03-04 15:14:36 +0800

    feat: create authentication landing pages
    Co-Authored-By: AlligatorW <99378933+AlligatorW@users.noreply.github.com>

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -14,26 +14,26 @@ export default function Home() {
           backgroundPosition: 'center'
         }}
       >
-        <div class="relative">
-          <div class="absolute inset-0 bg-black opacity-70"></div>
-          <div class="py-8 px-32">
-            <div class="flex items-center gap-16">
-              <Image src="/backgrounds/NYU-logo.png" width="100" height="39" alt="NYU Logo" class="z-10"/>
-              <h3 class="text-white z-10 font-light">Calendar</h3>
-              <h3 class="text-white z-10 font-light">Announcements</h3>
-              <h3 class="text-white z-10 font-light">Messages</h3>
+        <div className="relative">
+          <div className="absolute inset-0 bg-black opacity-70"></div>
+          <div className="py-8 px-32">
+            <div className="flex items-center gap-16">
+              <Image src="/backgrounds/NYU-logo.png" width="100" height="39" alt="NYU Logo" className="z-10"/>
+              <h3 className="text-white z-10 font-light">Calendar</h3>
+              <h3 className="text-white z-10 font-light">Announcements</h3>
+              <h3 className="text-white z-10 font-light">Messages</h3>
             </div>
-            <div class="flex items-center justify-between py-8">
-              <h1 class="text-white font-bold text-5xl z-10">Welcome to Darkspace!</h1>
-              <h3 class="text-white text-xl z-10">{currentDate}</h3>
+            <div className="flex items-center justify-between py-8">
+              <h1 className="text-white font-bold text-5xl z-10">Welcome to Darkspace!</h1>
+              <h3 className="text-white text-xl z-10">{currentDate}</h3>
             </div>
           </div>
         </div>
       </nav>
-      <div class="bg-white">
-        <div class="flex items-center justify-between py-8 px-32">
-          <h1 class="font-bold text-2xl">Current Courses</h1>
-          <h2 class="font-light text-xl">Spring 2024</h2>
+      <div className="bg-white">
+        <div className="flex items-center justify-between py-8 px-32">
+          <h1 className="font-bold text-2xl">Current Courses</h1>
+          <h2 className="font-light text-xl">Spring 2024</h2>
         </div>
         <Courses 
           coursename="Software Engineering"
