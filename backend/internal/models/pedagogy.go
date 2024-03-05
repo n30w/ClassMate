adebac6fc86cadb92c0b1bee41d2b7b16b6dafb4
n30w <hello@neoalabastro.com>
2024-03-06 03:40:17 +0800

    feat(go): flesh out backend models
    add JSON tags as well.
    
    Co-Authored-By: Michelle <125943298+hypnosushi@users.noreply.github.com>

--- backend/internal/models/pedagogy.go
+++ backend/internal/models/pedagogy.go
@@ -2,33 +2,59 @@ package models
 
 import "time"
 
+type Post struct {
+	Title       string
+	Uuid        string
+	Description string
+	Media       []Media
+	Date        time.Time
+	Owner       User
+}
+
 type Assignment struct {
-	Name        string     `json:"name"`
-	Uuid        string     `json:"uuid"`
-	Media       Media      `json:"media"`
-	Discussion  Discussion `json:"discussion"`
-	DueDate     time.Time  `json:"due_date"`
-	Description string     `json:"description"`
+	Post       Post
+	Submission []Submission
+	Feedback   string
+	Grade      int
+	DueDate    time.Time `json:"due_date"`
+}
+
+func (a Assignment) addSubmission(submission Submission) {
+	a.Submission = append(a.Submission, submission)
+}
+
+type Submission struct {
+	User           User
+	FileType       string
+	SubmissionTime time.Time
+	OnTime         bool
 }
 
 type Course struct {
 	Name        string         `json:"name"`
 	Uuid        string         `json:"uuid"`
 	Discussions [10]Discussion `json:"discussions"`
-	Teachers    []Teacher      `json:"teachers"`
-	Roster      []Student      `json:"roster"`
+	Teachers    []User         `json:"teachers"`
+	Roster      []User         `json:"roster"`
 	Assignments []Assignment   `json:"assignments"`
 	Archived    bool           `json:"archived"`
 }
 
-// Contains anything related to communications,
+// Discussion contains anything related to communications,
 // such as discussion posts and user messages.
 type Discussion struct {
-	Name            string   `json:"name"`
-	Description     string   `json:"description"`
-	Participants    []User   `json:"participants"`
-	MediaReferences []Media  `json:"media_references"`
-	CommentThreads  []string `json:"comment_threads"`
+	Post     Post
+	Comments []Comment
+}
+
+func (d Discussion) addComment(comment Comment) {
+	d.Comments = append(d.Comments, comment)
+}
+
+// TODO: Linked lists
+type Comment struct {
+	Post    Post
+	Replies []Comment
 }
 
 type Project struct {
@@ -38,4 +64,6 @@ type Project struct {
 	MediaReferences []Media    `json:"media_references"`
 	Members         []User     `json:"members"`
 	Discussion      Discussion `json:"discussion"`
+
+	ac *accessControl
 }
--- backend/internal/models/users.go
+++ backend/internal/models/users.go
@@ -1,26 +1,109 @@
 package models
 
 import (
+	"errors"
+	"fmt"
 	"time"
 )
 
+type member uint8
+
+const (
+	STUDENT member = iota
+	TEACHER
+	ADMIN
+)
+
 type User struct {
-	Username       string    `json:"username"`
-	Password       string    `json:"password"`
-	Uuid           string    `json:"uuid"`
-	JoinDate       time.Time `json:"join_date"`
-	ModifiedDate   time.Time `json:"modified_date"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+
+	// The NetID serves as a UUID.
+	Netid string `json:"netid"`
+
+	// General user data
 	FullName       string    `json:"full_name"`
 	ProfilePicture Media     `json:"profile_picture"`
+	JoinDate       time.Time `json:"join_date"`
+	ModifiedDate   time.Time `json:"modified_date"`
 	Projects       []Project `json:"projects"`
 	Courses        []Course  `json:"courses"`
 	Bio            string    `json:"bio"`
+
+	ac *accessControl
 }
 
-type Student struct {
-	User User
+func (u User) createDiscussion(title string, description string, media []Media, date time.Time) Discussion {
+	return Discussion{
+		Name:            title,
+		Description:     description,
+		MediaReferences: media,
+	}
 }
 
-type Teacher struct {
-	User User
+type userConfig struct {
+	creds [4]credentials
 }
+
+// func newUserConfig() userConfig {
+// 	return userConfig{
+// 		creds: [4]credentials{
+// 			password{},
+// 			username("john"),
+// 			netid("rra9981"),
+// 			email("123@yahoo.com"),
+// 		},
+// 	}
+// }
+
+func (u userConfig) valid() error {
+	for _, v := range u.creds {
+		if err := v.valid(); err != nil {
+			return fmt.Errorf("invalid user configuration: %s", err)
+		}
+	}
+	return nil
+}
+
+type password struct{ value []byte }
+
+func (p password) valid() error {
+	valid := false
+	if !valid {
+		return errors.New("password does not match criteria")
+	}
+	return nil
+}
+
+type username string
+
+func (u username) valid() error {
+	// if username is already in DB, return false
+	valid := false
+	if !valid {
+		return errors.New("username already taken")
+	}
+	return nil
+}
+
+type netid string
+
+func (n netid) valid() error {
+	valid := false
+	if !valid {
+		return errors.New("netid already in use")
+	}
+	return nil
+}
+
+type email string
+
+func (e email) valid() error {
+	valid := false
+	if !valid {
+		return errors.New("email already in use")
+	}
+	return nil
+}
+
+// Instantiate new users in database and session.
