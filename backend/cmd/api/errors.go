a4ca3438b0bf29f3c89cecf7e982037cd94631dc
n30w <hello@neoalabastro.com>
2024-03-06 03:44:48 +0800

    refactor(go): small change to serverError

--- backend/cmd/api/errors.go
+++ backend/cmd/api/errors.go
@@ -1,6 +1,9 @@
 package main
 
-import "net/http"
+import (
+	"errors"
+	"net/http"
+)
 
 // Methods in this file define error handling functions.
 // logError wraps the app.logger.Print() method in order to
@@ -19,8 +22,8 @@ func (app *application) logError(r *http.Request, err error) {
 	app.logger.Print(err)
 }
 
-// internalServerError is a 500 status error.
-func (app *application) internalServerError(w http.ResponseWriter, r *http.Request, err error) {
+// serverError returns a 400 Bad Request. This is called when JSON is messed up.
+func (app *application) serverError(w http.ResponseWriter, r *http.Request, err error) {
 	app.logError(r, err)
-	http.Error(w, "The server could not process your request", http.StatusInternalServerError)
+	http.Error(w, err.Error(), http.StatusBadRequest)
 }
