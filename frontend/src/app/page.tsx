17c12071de376883905e4f242022486368f7c66f
reesedychiao <83895576+reesedychiao@users.noreply.github.com>
2024-03-06 14:02:16 +0800

    feat: update dashboard page
    Co-Authored-By: AlligatorW <99378933+AlligatorW@users.noreply.github.com>

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,52 +1,78 @@
+"use client";
+
 import Image from "next/image";
 import Courses from "./components/Courses";
+import React, { useState } from "react";
+import CreateCourse from "./components/CreateCourse";
 
 export default function Home() {
-
   const currentDate = new Date().toLocaleDateString();
+  const [isCreatingCourse, setIsCreatingCourse] = useState(false);
+
+  const handleClick = (e: any) => {
+    setIsCreatingCourse(true);
+  };
 
   return (
     <div>
       <nav
         style={{
-          backgroundImage: `url('/backgrounds/dashboard-bg.png')`,
-          backgroundSize: 'cover',
-          backgroundPosition: 'center'
+          backgroundImage: `url('/backgrounds/dashboard-bg.jpeg')`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
         }}
       >
         <div className="relative">
           <div className="absolute inset-0 bg-black opacity-70"></div>
           <div className="py-8 px-32">
-            <div className="flex items-center gap-16">
-              <Image src="/backgrounds/NYU-logo.png" width="100" height="39" alt="NYU Logo" className="z-10"/>
-              <h3 className="text-white z-10 font-light">Calendar</h3>
-              <h3 className="text-white z-10 font-light">Announcements</h3>
-              <h3 className="text-white z-10 font-light">Messages</h3>
-            </div>
-            <div className="flex items-center justify-between py-8">
-              <h1 className="text-white font-bold text-5xl z-10">Welcome to Darkspace!</h1>
-              <h3 className="text-white text-xl z-10">{currentDate}</h3>
+            <div className="flex items-center gap-4">
+              <Image
+                src="/backgrounds/NYU-logo.png"
+                width="150"
+                height="39"
+                alt="NYU Logo"
+                className="z-10"
+              />
+              <Image
+                src="/backgrounds/darkspace.png"
+                width="200"
+                height="39"
+                alt="Darkspace Logo"
+                className="z-10"
+              />
             </div>
           </div>
         </div>
       </nav>
       <div className="bg-white">
         <div className="flex items-center justify-between py-8 px-32">
-          <h1 className="font-bold text-2xl">Current Courses</h1>
-          <h2 className="font-light text-xl">Spring 2024</h2>
+          <h1 className="font-bold text-2xl">Spring 2024</h1>
+          <button
+            className="rounded-full bg-black text-white font-light px-4 py-2 h-12"
+            onClick={handleClick}
+          >
+            + Create Course
+          </button>
         </div>
-        <Courses 
+        <Courses
           coursename="Software Engineering"
           professor="Xu, Lihua"
           time="Tue,Thu 3.45 PM - 5.00 PM"
           loc="Room S311"
         />
-        <Courses 
+        <Courses
           coursename="Software Engineering"
           professor="Xu, Lihua"
           time="Tue,Thu 3.45 PM - 5.00 PM"
           loc="Room S311"
         />
+        {isCreatingCourse && (
+          <CreateCourse
+            onClose={() => {
+              setIsCreatingCourse(false);
+            }}
+          />
+        )}
       </div>
     </div>
   );
