d39b79f7ca7ed9b29e2b2bfe2267a9aea60284b5
n30w <hello@neoalabastro.com>
2024-03-18 18:50:50 +0800

    refactor: formatDate into helpers

--- frontend/src/components/Discussions.tsx
+++ frontend/src/components/Discussions.tsx
@@ -1,22 +1,15 @@
+import formattedDate from "@/lib/helpers/formattedDate";
 import React from "react";
 
 const Discussions: React.FC = () => {
-  const currentDate = new Date();
-
-  const formattedDate = currentDate
-    .toLocaleDateString("en-US", {
-      month: "2-digit",
-      day: "2-digit",
-      year: "numeric",
-    })
-    .replace(/\//g, "-");
+  const date: string = formattedDate();
 
   return (
     <div className="bg-black bg-opacity-70 p-8 mt-8">
       <h2 className="text-white font-bold">
         Balancing Innovation and Maintenance
       </h2>
-      <h3 className="text-white">Neo Alabastro posted on {formattedDate}</h3>
+      <h3 className="text-white">Neo Alabastro posted on {date}</h3>
       <a
         href=""
         className="text-white font-light flex justify-end text-sm pt-2"
