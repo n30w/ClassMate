5f18c29aad603404d0da14b01a3aebe4da97b251
n30w <hello@neoalabastro.com>
2024-03-18 18:45:58 +0800

    feat+refactor: add helpers folder

---
+++ frontend/src/lib/helpers/formattedDate.ts
@@ -0,0 +1,11 @@
+const formattedDate = () => {
+  return new Date()
+    .toLocaleDateString("en-US", {
+      month: "2-digit",
+      day: "2-digit",
+      year: "numeric",
+    })
+    .replace(/\//g, "-");
+};
+
+export default formattedDate;
