b8f0190c1957c723ac2402a17ce3f288f98db4aa
n30w <rra9981@nyu.edu>
2024-04-28 15:58:38 +0800

    fix: remove panicRecovery

--- backend/cmd/api/server.go
+++ backend/cmd/api/server.go
@@ -19,6 +19,8 @@ func (app *application) server() error {
 		),
 	)
 
+	handler = app.enableCORS(app.rateLimit(app.routes()))
+
 	srv := &http.Server{
 		Addr:         fmt.Sprintf(":%d", app.config.port),
 		Handler:      handler,
