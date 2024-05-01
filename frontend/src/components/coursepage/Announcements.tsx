70e443bc48251f75ec4466ba0686a3e59b9d124d
n30w <rra9981@nyu.edu>
2024-05-02 02:01:26 +0800

    feat(front): router refresh on add

--- frontend/src/components/coursepage/Announcements.tsx
+++ frontend/src/components/coursepage/Announcements.tsx
@@ -4,14 +4,24 @@ import React, { useState, useEffect } from "react";
 import CreateAnnouncement from "./CreateAnnouncement";
 import AddButton from "@/components/buttons/AddButton";
 import AnnouncementDisplay from "./AnnouncementDisplay";
-import axios from "axios";
-import { revalidatePath } from "next/cache";
+import { useRouter, usePathname } from "next/navigation";
 
 interface props {
   courseId: string;
 }
 
 const Announcements: React.FC<props> = (props: props) => {
+  const router = useRouter();
+  const pathName = usePathname();
+
+  // Function is a variation of: https://www.joshwcomeau.com/nextjs/refreshing-server-side-props/
+  // and https://nextjs.org/docs/app/api-reference/functions/use-pathname
+  // and https://github.com/vercel/next.js/discussions/62146
+  const refreshData = () => {
+    router.push(pathName);
+    window.location.reload();
+  };
+
   const [isCreatingAnnouncement, setIsCreatingAnnouncement] = useState(false);
   const [isTeacher, setIsTeacher] = useState(false);
   const [token, setToken] = useState("");
@@ -58,6 +68,7 @@ const Announcements: React.FC<props> = (props: props) => {
         <CreateAnnouncement
           onClose={() => {
             setIsCreatingAnnouncement(false);
+            refreshData();
           }}
           token={token}
           params={{ id: props.courseId }}
