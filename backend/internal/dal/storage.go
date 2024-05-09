0a36fc45f89c7c57e28261e4e03a4e2cfab2c0e8
n30w <rra9981@nyu.edu>
2024-05-09 23:30:20 +0800

    feat: LocalVolume methods

--- backend/internal/dal/storage.go
+++ backend/internal/dal/storage.go
@@ -4,4 +4,32 @@
 
 package dal
 
+import (
+	"io"
+	"os"
+)
+
 type LocalVolume struct{}
+
+func NewLocalVolume() *LocalVolume {
+	return &LocalVolume{}
+}
+
+// CreateFile makes a new file and returns it. Does not automatically close!
+func (lv *LocalVolume) CreateFile(path string) (*os.File, error) {
+	f, err := os.Create(path)
+	if err != nil {
+		return nil, err
+	}
+
+	return f, nil
+}
+
+func (lv *LocalVolume) CopyFile(f1 io.Writer, f2 io.Reader) error {
+	_, err := io.Copy(f1, f2)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
