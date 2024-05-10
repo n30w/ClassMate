9f8b2dc0b72ee684e0edb809cdd47b4cacac1add
n30w <rra9981@nyu.edu>
2024-05-10 16:19:40 +0800

    feat: update FileService, save operation

--- backend/internal/domain/file.go
+++ backend/internal/domain/file.go
@@ -7,7 +7,7 @@ import (
 )
 
 type FileStore interface {
-	CreateFile(path string) (*os.File, error)
+	CreateFile(path string) (*os.File, string, error)
 	CopyFile(f1 io.Writer, f2 io.Reader) error
 }
 
@@ -18,19 +18,20 @@ type FileService struct {
 func NewFileService(store FileStore) *FileService { return &FileService{store: store} }
 
 // Save saves a file to disk. This is used for incoming
-// files from the handlers.
-func (fs *FileService) Save(name string, in multipart.File) error {
-	f, err := fs.store.CreateFile(name)
+// files from the handlers. It returns a path to where the
+// file was saved and an error.
+func (fs *FileService) Save(name string, in multipart.File) (string, error) {
+	f, p, err := fs.store.CreateFile(name)
 	if err != nil {
-		return err
+		return "", err
 	}
 
 	defer f.Close()
 
 	err = fs.store.CopyFile(f, in)
 	if err != nil {
-		return err
+		return "", err
 	}
 
-	return nil
+	return p, nil
 }
