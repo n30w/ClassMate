ba2a0513ad155067089d5fc627709c8ef1c2e418
n30w <rra9981@nyu.edu>
2024-05-13 18:03:27 +0800

    feat: add headers to CORS

--- backend/cmd/api/middleware.go
+++ backend/cmd/api/middleware.go
@@ -90,6 +90,22 @@ func (app *application) enableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(
 		func(w http.ResponseWriter, r *http.Request) {
 			w.Header().Set("Access-Control-Allow-Origin", "*")
+			w.Header().Set(
+				"Access-Control-Allow-Methods",
+				"GET, POST, PUT, DELETE, OPTIONS",
+			)
+			w.Header().Set(
+				"Access-Control-Allow-Headers",
+				"Content-Type, Authorization",
+			)
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
+
+			// If the request is for the OPTIONS method, return immediately with a 200 status
+			// as this is a preflight request
+			if r.Method == "OPTIONS" {
+				w.WriteHeader(http.StatusOK)
+				return
+			}
 
 			next.ServeHTTP(w, r)
 		},
